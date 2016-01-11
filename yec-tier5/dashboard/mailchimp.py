e9238113880fd09382b1b51b8860997e3e39857c
sinanuozdemir <sinan.u.ozdemir@gmail.com>
2016-01-10 20:40:11 -0800

    first?

--- yec-tier5/dashboard/mailchimp.py
+++
@@ -1,3 +0,0 @@
-import requests
-
-api_root = "https://" + shard + ".api.mailchimp.com/3.0/"
--- yec-tier5/legion/yec.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('/Users/sinanozdemir/Downloads/YEC.csv')
-
-emails = map(lambda x: str(x.lower()), list(df['Email'].dropna()))
-
-
-emails[1031]
-
